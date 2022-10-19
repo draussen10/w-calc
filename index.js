4132eac9ad1ec809accbe3825d741dab5be2b631
draussen10 <a.a.boev@bk.ru>
2022-10-19 17:25:30 +0300

    third+ commit

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const switchItems = [...document.querySelectorAll('.switch-item')]
 
+const $ul = document.querySelector('.text')
+
+const arrayLi = [
+	'Пожар, взрыв, удар молнии, стихийные бедствия, противоправные действия третьих лиц;',
+	'Воздействие жидкости, посторонних предметов, электрического тока, повреждение в результате ДТП;',
+	'Внешнее механическое воздействие, в том числе случайное падение (этот и предыдущие риски — на первый год действия полиса);',
+	'Поломка имущества (условия, аналогичные гарантийным условиям от производителя) (на второй год действия полиса по окончании гарантии от производителя).'
+]
+
 let activeSwitchItemNum = 0
 
 const switchItemsHandler = event => {
@@ -13,3 +22,11 @@ const switchItemsHandler = event => {
 switchItems.forEach(function(node) {
 	node.addEventListener('click', switchItemsHandler)
 })
+
+
+document.addEventListener('DOMContentLoaded', function() {
+	$ul.innerHTML = ''
+	for(li of arrayLi) {
+		$ul.innerHTML += `<li>${li}</li>`
+	}
+}, false);
